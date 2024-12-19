8bf152dfd65e80982695ffd9f070ca8424eccd91
MumtazAysha <139667400+MumtazAysha@users.noreply.github.com>
2024-12-19 13:03:09 +0530

    Update dbconf.php

--- dbconf.php
+++ dbconf.php
@@ -2,7 +2,7 @@
 $hostname = "127.0.0.1";
 $username = "root";
 $password = "mariadb";
-$dbname = "StyleVoguedb";
+$dbname = "usersdb";
 
 $connection = mysqli_connect($hostname,$username,$password,$dbname);
 ?>
