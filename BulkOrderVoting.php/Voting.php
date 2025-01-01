d70a66e8583d76434b4eefed42cd2f49d70a7b66
MumtazAysha <139667400+MumtazAysha@users.noreply.github.com>
2025-01-01 14:22:26 +0000

    Edited

--- BulkOrderVoting.php/Voting.php
+++ BulkOrderVoting.php/Voting.php
@@ -9,25 +9,10 @@
 </head>
 <body>
     <!-- Header Section -->
-    <header class="text-white p-3" style="background-color: rgb(23, 167, 224);">
-        <div class="container d-flex justify-content-between align-items-center">
-            <h1 class="logo">StyleVogue</h1>
-            <nav>
-                <ul class="nav">
-                    <li class="nav-item"><a href="#" class="nav-link text-white"><h5>Home</h5><img src="Images/326656_home_icon.png" alt="Home" height="50px"></a></li>
-                    <li class="nav-item"><a href="#" class="nav-link text-white"><h5>About</h5><img src="Images/info-button.png" alt="About" height="50px"></a></li>
-                    <li class="nav-item"><a href="#" class="nav-link text-white"><h5>Collections</h5><img src="Images/8672974_ic_fluent_collections_filled_icon.png" alt="collections" height="50px"></a></li>
-                    <li class="nav-item"><a href="#" class="nav-link text-white"><h5>Group Interaction</h5><img src="Images/team.png" alt="Group interaction" height="50px"></a></li>
-                    <li class="nav-item"><a href="#" class="nav-link text-white"><h5>Bulk Order Voting</h5><img src="Images/voting-box.png" alt="Bulk order voting" height="50px"></a></li>
-                    <li class="nav-item"><a href="#" class="nav-link text-white"><h5>Contact</h5><img src="Images/phone.png" alt="Contact" height="50px"></a></li>
-                    <li class="nav-item"><a href="login.php" class="nav-link text-white"><h5>Log in</h5><img src="Images/enter.png" alt="Log in" height="50px"></a></li>
-                </ul>
-            </nav>
-        </div>
-    </header>
+    <?php include 'header.php'; ?>
 
     <!-- Group Interaction Section -->
-    <?php include 'header.php'; ?>
+    
     <section class="group-interaction py-5 bg-light">
         <div class="container text-center">
             <h2>Group Interaction</h2>
